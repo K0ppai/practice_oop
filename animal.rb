26c9acc84690407c42c922c44098c54b9d11a0d2
koppai <psthu.koppai@gmail.com>
2023-08-28 19:16:27 +0630

    Add functions in Animal class

--- animal.rb
+++ animal.rb
@@ -5,4 +5,37 @@ class Animal
     @number_of_legs = number_of_legs
     @type = type
   end
+
+  def id
+    @id
+  end
+
+  def type
+    @type
+  end
+
+  def number_of_legs
+    @number_of_legs
+  end
+
+  attr_accessor :name
+
+  def speak
+    if @type == "dog"
+      "Woof, woof"
+    elsif @type == "spider"
+      "..."
+    end
+  end
+end
+
+class Dog < Animal
+  def initialize(color, name = "Unknown")
+    super("dog", 4, name)
+    @color = color
+  end
+
+  def bring_a_stick
+    "Here is your stick: ---------"
+  end
 end
