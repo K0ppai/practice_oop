245b329778b0ad7a919f6d32c3fe9f6c6fa41133
koppai <psthu.koppai@gmail.com>
2023-08-28 19:16:48 +0630

    Move Dog class to dog.rb file

--- animal.rb
+++ animal.rb
@@ -28,14 +28,3 @@ class Animal
     end
   end
 end
-
-class Dog < Animal
-  def initialize(color, name = "Unknown")
-    super("dog", 4, name)
-    @color = color
-  end
-
-  def bring_a_stick
-    "Here is your stick: ---------"
-  end
-end
